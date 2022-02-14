bd2509926869d33cc6e14152a2e721c35bb1f91a
Akio Tanaka <akimaron2828@icloud.com>
2022-02-14 09:50:15 +0900

    test success, access and title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,14 +3,27 @@ require "test_helper"
 # StaticPageのテストが2つ書かれてる
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
+  # 変数も使える
+  def setup
+    @base_title = "Ruby on Rails Tutorial App"
+  end
+
   # Homeのテスト。getリクエストをHomeにしたら成功になるよね？
   test "should get home" do
     get static_pages_home_url
-    assert_response :success
+    assert_response :success # ちゃんとアクセスできる？
+    assert_select "title", "Home | Ruby on Rails Tutorial App" # このページのtitleこれ？
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | #{@base_title}"
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 end
