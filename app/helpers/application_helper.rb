69aa5b835b39dc36cf2357e0ef215360412b0f90
Akio Tanaka <akimaron2828@icloud.com>
2022-02-15 15:37:14 +0900

    add link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  # ここに書くと、自動で全fileにincludeされる！
+  def full_title(page_title="")
+    base_title = "Ruby on Rails Tutorial App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
