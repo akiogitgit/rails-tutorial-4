49ec766863400d2f916aa4152951cb5f17414cd8
Akio Tanaka <akimaron2828@icloud.com>
2022-02-17 10:25:14 +0900

    add validates email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  # //の間に書く。iは大小関係ないオプション、\Aで始まり\zで終わり
+  # \wで英数字と_、\dで数字  \. で .  +は1以上、*は0以上
+  # [\w+-.]+で英数字_+.一つ以上、[a-z\d\-]
+  #uniqueness: true # 同じのはダメよ
+
+
+  # 制約 ( : の位置気を付けて)
+  validates :name, presence: true, length: { maximum:50 }
+  validates :email, presence: true, length: { maximum:255 },
+  format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }, # .を連続しない
+  uniqueness: { case_sensitive: false } # 大小関係なく同じのダメよ
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,13 +6,56 @@ class UserTest < ActiveSupport::TestCase
     @user = User.new(name: "Example User", email: "user@example.com")
   end
 
-  # test "should be valid" do
-  #   assert @user.valid?
-  # end
+  test "should be valid" do
+    assert @user.valid?
+  end
 
-  # 新しいuser.nameでテスト。空文字
-  # test "name should be present" do
-  #   @user.name ="   "
-  #   assert_not @user.valid?
-  # end
+  # 空文字は許さないよね？(assert_notで逆の意味)
+  test "name should be present" do
+    @user.name =""
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "  "
+    assert_not @user.valid?
+  end
+
+  # name をこの長さにしたらダメだよね？
+  test "name should not be too long" do
+    @user.name = "a"*51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a"*244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  # 有効なメールのテスト
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+      first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |address|
+      @user.email = address
+      assert @user.valid?, "#{address.inspect} should be valid"
+    end
+  end
+  
+  # このemailだとエラーだよね？
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[aa@aa..a user@example,com user_at_foo.org user.name@example]
+    invalid_addresses.each do |address|
+      @user.email = address
+      assert_not @user.valid?, "#{address.inspect} should be invalid"
+    end
+  end
+
+  # 同じemailはダメだよね？
+  test "email addressses should be unique" do
+    duplicate_user = @user.dup # dupでダブってるuser
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 end
