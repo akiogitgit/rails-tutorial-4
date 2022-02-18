af57dd07b75a8ea2dc26cdd0e0a92e2dd11e7c31
Akio Tanaka <akimaron2828@icloud.com>
2022-02-18 15:22:24 +0900

    add form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,26 @@ class UsersController < ApplicationController
     # debugger # ターミナルで出てくる。@user.nameとか使える Ctr+D
   end 
 
+  # formが実行されたら、この２つが実行される
   def new
     # debugger
+    @user = User.new
   end
 
+  def create
+    # @user = User.new(params[:user]) # postリクエストから情報を受け取る。だが、これだと怪しい情報も付与されるかも。必要な情報だけ受け取るようにする必要あり
+    @user = User.new(user_params)
+    if @user.save
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  # 外部から使えないように、必要ない情報は受け取らないようにする。
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                :password_confirmation)
+  end
 end
