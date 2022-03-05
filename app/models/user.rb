40740333ffe61a43c2441e25a705049830e6de12
Akio Tanaka <akimaron2828@icloud.com>
2022-03-05 09:54:16 +0900

    users controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum:20 }
-  validates :email, presence: true, length: { maximum:20 }
-  
+  validates :email, presence: true, length: { maximum:50 }
+  validates :password, presence: true, length: { minimum: 6 }
+  has_secure_password
 end
